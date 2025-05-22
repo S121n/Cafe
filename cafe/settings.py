95c20a1f1f14b1e1f0f7645dbcb3cca2750639d8
ASUS <ilia.ahmadi.swim.820@gmail.com>
2025-05-22 20:27:26 +0330

    Mr U , ICA

--- cafe/settings.py
+++ cafe/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    "menu",
+    'menu',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +118,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'Static'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- menu/views.py
+++ menu/views.py
@@ -4,4 +4,4 @@ from .models import Category
 
 def menu_view(request):
     categories = Category.objects.prefetch_related('items').all()
-    return render(request, 'menu.html', {'category': categories})
+    return render(request, 'menu.html', {'categories': categories})
